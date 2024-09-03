f38023caf3bb12006a93b1b9f8bb7496b3ebec6d
BeshoyAshraf1 <https://github.com/BeshoyAshraf1>
2024-09-03 11:09:26 -0700

    task4

---
+++ Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double  Degree { get; set; }
+
+        public double MinDegree { get; set; }
+
+        public virtual ICollection<Instractor> Instractors { get; set; }
+
+        public virtual ICollection<Courseresult> Courseresults { get; set; }
+
+
+    }
+}
